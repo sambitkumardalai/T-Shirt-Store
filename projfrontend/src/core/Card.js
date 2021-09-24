6df3f35d8199b881adf556329b3797e83093ca62
sambit <sambitskd3@gmail.com>
2021-09-24 07:47:00 +0530

    tshirt added to cart page

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -1,33 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ImageHelper from "./helper/ImageHelper";
+import { Redirect } from "react-router-dom";
+import { addItemToCart } from "./helper/cartHelper";
 
-const Card = () => {
+const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
+  const [redirect, setRedirect] = useState(false);
+  const [count, setCount] = useState(product.count);
+
+  const cartTitle = product ? product.name : "A photo from pexels";
+  const cartDescrption = product ? product.description : "Default description";
+  const cartPrice = product ? product.price : "DEFAULT";
+
+  const addToCart = () => {
+    addItemToCart(product, () => setRedirect(true));
+  };
+
+  const getARedirect = (redirect) => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
+
+  const showAddToCart = (addtoCart) => {
+    return (
+      addtoCart && (
+        <button
+          onClick={addToCart}
+          className="btn btn-block btn-outline-success mt-2 mb-2"
+        >
+          Add to Cart
+        </button>
+      )
+    );
+  };
+
+  const showRemoveFromCart = (removeFromCart) => {
+    return (
+      removeFromCart && (
+        <button
+          onClick={() => {}}
+          className="btn btn-block btn-outline-danger mt-2 mb-2"
+        >
+          Remove from cart
+        </button>
+      )
+    );
+  };
   return (
     <div className="card text-white bg-dark border border-info ">
-      <div className="card-header lead">A photo from pexels</div>
+      <div className="card-header lead">{cartTitle}</div>
       <div className="card-body">
-        <ImageHelper />
+        {getARedirect(redirect)}
+        <ImageHelper product={product} />
         <p className="lead bg-success font-weight-normal text-wrap">
-          this photo looks great
+          {cartDescrption}
         </p>
-        <p className="btn btn-success rounded  btn-sm px-4">$ 5</p>
+        <p className="btn btn-success rounded  btn-sm px-4">$ {cartPrice}</p>
         <div className="row">
-          <div className="col-12">
-            <button
-              onClick={() => {}}
-              className="btn btn-block btn-outline-success mt-2 mb-2"
-            >
-              Add to Cart
-            </button>
-          </div>
-          <div className="col-12">
-            <button
-              onClick={() => {}}
-              className="btn btn-block btn-outline-danger mt-2 mb-2"
-            >
-              Remove from cart
-            </button>
-          </div>
+          <div className="col-12">{showAddToCart(addtoCart)}</div>
+          <div className="col-12">{showRemoveFromCart(removeFromCart)}</div>
         </div>
       </div>
     </div>
